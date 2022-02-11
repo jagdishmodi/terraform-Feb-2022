17960b6ca61b969fa287085d3316e2f391709fa1
jagdishmodi <48287274+jagdishmodi@users.noreply.github.com>
2022-02-11 11:33:11 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "s3_ec21" {
     first_vm = "ami-033b95fb8079dc481"
     second_vm = "ami-04505e74c0741db8d"
   }
-  user_data = file("./app1-install.sh")
+ 
   instance_type = "t2.micro"
   
   tags = {
