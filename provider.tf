18bd32fef98dcde71d6e7681eff702b69f5e1a3c
jagdishmodi <48287274+jagdishmodi@users.noreply.github.com>
2022-03-24 12:31:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 variable "env" {
 
     description = "The env for tags"
